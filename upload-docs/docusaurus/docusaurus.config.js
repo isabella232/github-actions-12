600235af88ee2ab7ce57f55f14e48cd1699a35c4
Toka Gelashvili <toka.gelashvili@visma.com>
2022-07-04 16:23:14 +0400

    EC-35119 Docs validation (#30)
    * Add docusaurus validation
    
    * Do not check MD file changes

---
+++ upload-docs/docusaurus/docusaurus.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'Documentation validation',
+  tagline: 'Documentation validation',
+  url: 'https://docs-test.private.e-conomic.ws',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  organizationName: 'e-conomic',
+  projectName: 'documentation validation',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/e-conomic/github-actions/tree/master/upload-docs',
+        }
+      },
+    ],
+  ],
+  plugins: [
+    require.resolve('@cmfcmf/docusaurus-search-local')
+  ]
+};
